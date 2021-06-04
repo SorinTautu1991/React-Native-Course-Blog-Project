bc5c9b0ce3f27f83df58139418375bc47332727f
SorinTautu1991 <sorin.tautu@gmail.com>
2021-06-04 17:27:11 +0300

    Installed React-Navigation dependencies

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
+import IndexScreen from "./src/screens/IndexScreen";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  { initialRouteName: "Index", defaultNavigationOptions: { title: "Blogs" } }
+);
+
+export default createAppContainer(navigator);
